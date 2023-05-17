3e2d2297a133e71609732904dfee638af8470fe4
Shaun O'Sullivan <shaunosullivan90@live.com>
2023-05-17 16:14:47 +1000

    Added an override to the fetch method to allow for sending the AntiForgery token with any fetch requests made, to re-configure security disabled throughout the UI learning experience

--- JokesMVC2023/wwwroot/js/site.js
+++ JokesMVC2023/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function advFetch(url, options) {
+
+    let verifyToken = document.querySelector('input[name="__RequestVerificationToken"]').value;
+
+    // define the options object if it has not been defined
+    if (options == undefined) {
+        options = {};
+    }
+
+    // define the headers within options
+    if (options.headers == undefined) {
+        options.headers = {};
+    }
+
+    // set the CSRF token if found
+    if (verifyToken != undefined) {
+        options.headers['RequestVerificationToken'] = verifyToken;
+    }
+
+    // set a custom header to note a fetch request
+    options.headers['x-fetch-request'] = "";
+
+
+    var promise = fetch(url, options)
+    return promise;
+
+}
